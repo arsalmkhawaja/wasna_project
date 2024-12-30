33edd1b5e887a632155e982da3d1c2bab2c76f09
[arsalmkhawaja] <arsalkh62@gmail.com>
2024-12-31 00:17:33 +0500

    Data adding and file modularity of data

---
+++ frontend/src/Data/DecorOptions.js
@@ -0,0 +1,4 @@
+const decorOptions = [
+    { name: "Traditional Decor", price: 5000 },
+  ];
+export default decorOptions;
---
+++ frontend/src/Data/PhotographyPackages.js
@@ -0,0 +1,83 @@
+const photographyPackages = [
+  {
+    type: "Economy Package",
+    packages: [
+      {
+        name: "Event Photography",
+        description: [
+          "Unlimited Soft Copies",
+          "Couple Photo Shoot & Family Shoot",
+          "Couple/Bride/Groom Photo shoot Video with light graphics",
+          "1 Photographer",
+        ],
+        price: "25000 For One Day",
+      },
+      {
+        name: "Event Videography",
+        description: [
+          "Event Coverage",
+          "Couple Shoot, Family Shoot, Gathering Shoot",
+          "Master Video + Highlights",
+          "1 Videographer",
+        ],
+        price: "25000 For One Day",
+      },
+    ],
+  },
+  {
+    type: "Silver Package",
+    packages: [
+      {
+        name: "Premium Photography",
+        description: [
+          "Up to 8 hours of coverage",
+          "2 Photographers",
+          "1000+ Edited Photos",
+          "Album and Online Gallery for 2 Years",
+          "Drone Photography",
+        ],
+        price: 30000,
+      },
+      {
+        name: "Premium Photography + Video",
+        description: [
+          "Up to 10 hours of coverage",
+          "2 Photographers & 1 Videographer",
+          "1500+ Edited Photos",
+          "Album, Online Gallery & Video Highlights",
+          "Drone Photography & Cinematic Video",
+        ],
+        price: 40000,
+      },
+    ],
+  },
+  {
+    type: "Luxury",
+    packages: [
+      {
+        name: "Luxury Full Day",
+        description: [
+          "Full-day coverage (up to 12 hours)",
+          "2 Photographers & 1 Videographer",
+          "2000+ Edited Photos",
+          "Album, Online Gallery & Video Highlights",
+          "Drone Photography & Live Streaming",
+        ],
+        price: 50000,
+      },
+      {
+        name: "Luxury Elite",
+        description: [
+          "Full-day coverage (up to 14 hours)",
+          "3 Photographers & 2 Videographers",
+          "2500+ Edited Photos",
+          "Luxury Album & Online Gallery for 3 Years",
+          "Drone Photography, Live Streaming & 4K Video",
+        ],
+        price: 70000,
+      },
+    ],
+  },
+];
+
+export default photographyPackages;
